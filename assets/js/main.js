4ed4774481be94bfbac0b122840e9b788a72648e
Yash Goyal <24110399@iitgn.ac.in>
2026-06-03 21:02:17 +0530

    Force-refresh script and projects data cache keys

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,7 +144,7 @@ filterButtons.forEach((btn) => {
   });
 });
 
-fetch("assets/data/projects.json?v=20260603-4")
+fetch("assets/data/projects.json?v=20260603-6")
   .then((res) => res.json())
   .then((data) => {
     state.projects = data.projects || [];
